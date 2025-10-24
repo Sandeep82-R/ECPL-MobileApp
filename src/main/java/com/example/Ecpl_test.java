753862fe6a7e209e4567c37b2f2d79420a57a96c
Sandeep82-R <sandeepr4046@gmail.com>
2025-10-24 15:41:22 +0530

    Again added the chrome options

--- src/main/java/com/example/Ecpl_test.java
+++ src/main/java/com/example/Ecpl_test.java
@@ -1,11 +1,8 @@
 package com.example;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -13,48 +10,52 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Ecpl_test {
-	WebDriver driver;
+    WebDriver driver;
     WebDriverWait wait;
-  @BeforeClass
-  public void setup() {
-	  WebDriverManager.chromedriver().setup();
-      driver = new ChromeDriver();
 
-      wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-  }
+    @BeforeClass
+    public void setup() {
+        System.out.println("Starting setup...");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage");
+        String tempDir = System.getProperty("java.io.tmpdir") + "/chrome_profile_" + System.currentTimeMillis();
+        options.addArguments("--user-data-dir=" + tempDir);
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        System.out.println("Setup complete. Browser initialized.");
+    }
 
-  @Test
-  public void loginTest() throws IOException {
-      System.out.println("Navigating to https://www.amazon.in/");
-      driver.get("https://www.amazon.in/");
-      try {
-          System.out.println("Waiting for search box...");
-          wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#twotabsearchtextbox"))).sendKeys("Testing 12333");
-          System.out.println("Entered text in search box.");
-          wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#twotabsearchtextbox"))).clear();
-          System.out.println("Cleared search box.");
-         
-      } catch (Exception e) {
-          System.err.println("Error in loginTest: " + e.getMessage());
-          throw e;
-      }
-  }
+    @Test
+    public void loginTest() throws IOException {
+        System.out.println("Navigating to https://www.amazon.in/");
+        driver.get("https://www.amazon.in/");
+        try {
+            System.out.println("Waiting for search box...");
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#twotabsearchtextbox"))).sendKeys("Testing 12333");
+            System.out.println("Entered text in search box.");
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#twotabsearchtextbox"))).clear();
+            System.out.println("Cleared search box.");
+        } catch (Exception e) {
+            System.err.println("Error in loginTest: " + e.getMessage());
+            throw e;
+        }
+    }
 
-  @AfterClass
-  public void tearDown() {
-      System.out.println("Tearing down...");
-      if (driver != null) {
-          driver.quit();
-      }
-      System.out.println("Browser closed.");
-  }
+    @AfterClass
+    public void tearDown() {
+        System.out.println("Tearing down...");
+        if (driver != null) {
+            driver.quit();
+        }
+        System.out.println("Browser closed.");
+    }
 }
